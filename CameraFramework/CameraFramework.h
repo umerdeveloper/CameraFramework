741f2485763adb5b01e66a1847c39f169d7f9a3c
Umer Khan <umar_s66@outlook.com>
2020-04-03 11:31:48 +0500

    Initial Commit

---
+++ CameraFramework/CameraFramework.h
@@ -0,0 +1,19 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Umer Khan on 03/04/2020.
+//  Copyright © 2020 Umer Khan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
